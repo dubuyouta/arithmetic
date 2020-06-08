880f5d885d0553eb0b66f4f301640619a7bce117
xiaobao.chen@zhaopin.com.cn <12345678>
2020-06-09 06:00:21 +0800

    两辆互换

--- src/main/java/com/example/arithmetic/arithmeticstu/link/Arithmetic2020060801.java
+++ src/main/java/com/example/arithmetic/arithmeticstu/link/Arithmetic2020060801.java
@@ -1,11 +1,21 @@
 package com.example.arithmetic.arithmeticstu.link;
 
 /**
+ *
+ * https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+ *
  * @author xiaobao.chen
  * Create at 2020-06-08
  */
 public class Arithmetic2020060801 {
 
+    /**
+     * 通过递归的方式进行两两替换。
+     *
+     *
+     * @param head
+     * @return
+     */
     public ListNode swapPairs(ListNode head) {
         if (head == null || head.next == null) {
             return null;
