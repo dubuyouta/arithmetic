b3cc13709e257532f6e824b8393a38495c633232
xiaobao.chen@zhaopin.com.cn <12345678>
2020-06-09 21:16:15 +0800

    合并K个排序链表

---
+++ src/main/java/com/example/arithmetic/arithmeticstu/link/Arithmetic2020060901.java
@@ -0,0 +1,37 @@
+package com.example.arithmetic.arithmeticstu.link;
+
+/**
+ * https://leetcode-cn.com/problems/merge-k-sorted-lists/
+ *
+ * @author xiaobao.chen
+ * Create at 2020-06-08
+ */
+public class Arithmetic2020060901 {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode ans = null;
+        for (int i = 0; i < lists.length; i++) {
+            ans = mergeTwoKLists(ans, lists[i]);
+        }
+        return ans;
+    }
+
+    public ListNode mergeTwoKLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        ListNode temp = new ListNode(-1);
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                temp.next = l1;
+                l1 = l1.next;
+            } else {
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+        temp.next = l1 == null ? l2 : l1;
+        return temp.next;
+    }
+}
